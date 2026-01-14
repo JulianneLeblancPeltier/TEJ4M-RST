bc2595e5430da20549407b92457ad21230849bc2
Julianne <julianne.leblanc-peltier1@stu.ocsb.ca>
2026-01-14 16:22:59 +0000

    added code.py file as test for pybadge

---
+++ code.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+"""
+Created by: Julianne Leblanc-Peltier
+Created on: January 14
+This program runs Tetris on the PyBadge
+"""
+
+def main_menu_scene() -> None:
+    """
+    This function is the main menu scene
+    """
+
+    print("Hello, World!")
+
+    # repeat forever, game loop
+    while True:
+        pass # just a placeholder for now
+
+if __name__ == "__main__":
+    main_menu_scene()
